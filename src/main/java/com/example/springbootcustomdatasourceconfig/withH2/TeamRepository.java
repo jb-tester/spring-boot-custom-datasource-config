706807c7b1b6426a528eafc51201a1304ec90c37
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-06-20 17:00:34 +0200

    add some query methods to repos

--- src/main/java/com/example/springbootcustomdatasourceconfig/withH2/TeamRepository.java
+++ src/main/java/com/example/springbootcustomdatasourceconfig/withH2/TeamRepository.java
@@ -2,6 +2,12 @@ package com.example.springbootcustomdatasourceconfig.withH2;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 public interface TeamRepository extends CrudRepository<Team,Integer> {
+    List<Team> findByTitle(String title);
+
+    List<Team> findByAmountGreaterThan(int value);
 }
+
--- src/main/java/com/example/springbootcustomdatasourceconfig/withMySQL/ContactRepository.java
+++ src/main/java/com/example/springbootcustomdatasourceconfig/withMySQL/ContactRepository.java
@@ -1,7 +1,14 @@
 package com.example.springbootcustomdatasourceconfig.withMySQL;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 
 public interface ContactRepository extends CrudRepository<Contact,Integer> {
+
+    @Override
+    Iterable<Contact> findAll();
+
+    List<Contact> findByLastname(String lastName);
+
 }
