17d783e0b918b2016fd0fd34e405766fc832bfba
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-06-20 17:18:07 +0200

    remove unused imports

--- src/main/java/com/example/springbootcustomdatasourceconfig/withPostgres/PostgresJpaConfiguration.java
+++ src/main/java/com/example/springbootcustomdatasourceconfig/withPostgres/PostgresJpaConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
