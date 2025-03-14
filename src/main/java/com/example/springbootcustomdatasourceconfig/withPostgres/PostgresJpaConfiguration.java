0e8c3d98e2b8c02e645909f145fe85abbac689d8
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-03-14 16:29:31 +0100

    use @EntityScan for entities location in postgres configuration

--- src/main/java/com/example/springbootcustomdatasourceconfig/withPostgres/PostgresJpaConfiguration.java
+++ src/main/java/com/example/springbootcustomdatasourceconfig/withPostgres/PostgresJpaConfiguration.java
@@ -1,12 +1,14 @@
 package com.example.springbootcustomdatasourceconfig.withPostgres;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScanPackages;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,25 +18,28 @@ import java.util.Objects;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
-  basePackages = "com.example.springbootcustomdatasourceconfig.withPostgres",
-  entityManagerFactoryRef = "postgresEntityManagerFactory",
-  transactionManagerRef = "postgresTransactionManager"
+        basePackages = "com.example.springbootcustomdatasourceconfig.withPostgres",
+        entityManagerFactoryRef = "postgresEntityManagerFactory",
+        transactionManagerRef = "postgresTransactionManager"
 )
+@EntityScan(basePackages = "com.example.springbootcustomdatasourceconfig.withPostgres")
 public class PostgresJpaConfiguration {
 
     @Bean
     public LocalContainerEntityManagerFactoryBean postgresEntityManagerFactory(
-      @Qualifier("postgresDataSource") DataSource dataSource,
-      EntityManagerFactoryBuilder builder) {
-        return builder
-          .dataSource(dataSource)
-          .packages("com.example.springbootcustomdatasourceconfig.withPostgres")
-          .build();
+            EntityScanPackages entityScanPackages,
+            @Qualifier("postgresDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        // Scan for entities in @EntityScan-detected packages
+        factoryBean.setPackagesToScan(entityScanPackages.getPackageNames().toArray(new String[0]));
+        return factoryBean;
     }
 
     @Bean
     public PlatformTransactionManager postgresTransactionManager(
-      @Qualifier("postgresEntityManagerFactory") LocalContainerEntityManagerFactoryBean postgresEMFactory) {
+            @Qualifier("postgresEntityManagerFactory") LocalContainerEntityManagerFactoryBean postgresEMFactory) {
         return new JpaTransactionManager(Objects.requireNonNull(postgresEMFactory.getObject()));
     }
 }
